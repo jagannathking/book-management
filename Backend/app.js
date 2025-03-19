08a3feed2a2b6caaac383662301f85f6e12bd66f
jagannathking <sethij832@gmail.com>
2025-03-19 16:43:04 +0530

    cors

--- Backend/app.js
+++ Backend/app.js
@@ -1,25 +1,25 @@
-const express = require('express')
+const express = require('express');
+const cors = require('cors');  // Import CORS
 const connectData = require('./config/database');
-const userRoutes = require('./routes/user.routes')
+const userRoutes = require('./routes/user.routes');
 const bookRoutes = require('./routes/book.routes');
 
 const app = express();
 
-// Connect data base
-connectData()
+// Connect database
+connectData();
 
-
-// middlewares
+// Middlewares
 app.use(express.json());
+app.use(cors()); 
 
-
-// all Routes
+// Routes
 app.use("/api/users", userRoutes);
 app.use("/api/books", bookRoutes);
 
-// Test Routes
+// Test Route
 app.get("/", (req, res) => {
-    res.status(200).json({message: "Healthy"})
-})
+    res.status(200).json({ message: "Healthy" });
+});
 
 module.exports = app;
